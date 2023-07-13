8c05b19dfc2ecd7d284c2fff5b0174b50bb8a091
ben-sappey <bsappey@ucsd.edu>
2023-07-13 08:42:07 -0500

    added documentation for gen_voigt.py and load.py

--- gen_voigt.py
+++ gen_voigt.py
@@ -3,22 +3,45 @@ from scipy.special import wofz
 import matplotlib.pyplot as plt
 
 def voigt(x, dx, sigma, gamma, amplitude):
-    """
-    Generate a Voigt profile at positions x, given the parameters sigma and gamma.
+    """Generate a Voigt profile at positions x, given the parameters sigma and gamma.
+
+    Args: 
+        x (arr): given to generate the Voigt profile
+        dx (float): location of the center of the profile
+        sigma(float): parameter characterizing the Voigt profile
+        gamma(float): parameter characterizing the Voigt profile
+        amplitude(float): amplitude of the Voigt profile
+    Returns:
+        v (arr): y values of the Voigt profile
+        
     """
     z = ((x-dx) + 1j*gamma) / (sigma * np.sqrt(2))
     v = amplitude*np.real(wofz(z)) / (sigma * np.sqrt(2*np.pi))
     return v
 
-def generate_voigt_profile(x,dx, sigma, gamma, amplitude, noise_std):
-    """
-    Generate a Voigt profile with given parameters at positions x, including Gaussian noise.
+def generate_voigt_profile(x,dx, sigma, gamma, amplitude, noise_std=0):
+    """Generate a Voigt profile with given parameters at positions x, including Gaussian noise.
+    
+    Args: 
+        x (arr): given to generate the Voigt profile
+        dx (float): location of the center of the profile
+        sigma(float): parameter characterizing the Voigt profile
+        gamma(float): parameter characterizing the Voigt profile
+        amplitude(float): amplitude of the Voigt profile
+        noise_std(float): standard deviation of noise to be applied to Voigt profile
+                        default is 0
+    
+    Returns:
+        y_with_noise (arr): Voigt profile with Gaussian noise
+
     """
     y = voigt(x, dx, sigma, gamma, amplitude)
     noise = np.random.normal(0, noise_std, len(x))
     y_with_noise = y + noise
     return y_with_noise
 
+#Example
+#------------------------------
 # Generate x values
 x = np.linspace(-10, 10, 1000)
 dx = 0
